5afc97956c53aa297a7a6125771dffd7ff1d8940
choi-ys <project.log.062@gmail.com>
2024-04-08 02:35:50 +0900

    build: logback 버전 수정
    AS-IS : 1.2.3
    TO-BE : 1.4.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation ("ch.qos.logback:logback-classic:1.2.3")
+    implementation ("ch.qos.logback:logback-classic:1.4.14")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
